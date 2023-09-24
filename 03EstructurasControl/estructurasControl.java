115fda2b6610758c0e276623b92c5e6c12b8f65b
CamerYT <a.hernandez.c965@edu.utc.mx>
2023-09-24 06:15:44 -0600

    Tiendita

--- 03EstructurasControl/estructurasControl.java
+++ 03EstructurasControl/estructurasControl.java
@@ -54,6 +54,82 @@ public class estructurasControl {
             System.out.println("13. Calculadora ");
             System.out.println("14. Salir ");
 
+
+            opcion = entrada.nextInt();
+
+            switch(opcion){
+                case 1:
+
+                    break;
+                case 2:
+                    //Decimal a binario
+                    System.out.println("Ingrese un numero positivo que desee convertir a binario: ");
+                    numBinario = entrada.nextInt();
+                    //saber que es positivo?
+                    
+                    if(numBinario > 0){
+                       //conozco cuantas veces se va a dividir entre dos?
+                       while(numBinario > 0){
+                            if(numBinario % 2==0){
+                                binario = "0" + binario;
+                            }else{
+                                binario = "1" + binario;
+                            }
+                            numBinario = (int)numBinario/2;
+                       }        
+
+                    }else if(numBinario == 0){
+                        binario = "0" + binario;
+                    }else {
+                        binario = "No se puede convertir un numero negativo solamente postivos";
+                    }
+                    System.out.println("El numero binario es: " + binario);
+                    break;
+                case 3:
+                
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("Bienvenido a la tiendita Pepsi");
+                    System.out.println("Por favor ingrese el numero de productos que desea comprar?");
+                    total = entrada.nextInt();
+
+                    for(int i = 1; i <= total; i++){
+                        System.out.println("Ingrese el nombre del producto: ");
+                        nombreProducto = entrada.next();
+                        System.out.println("Ingrese el precio del producto: ");
+                        precio = entrada.nextFloat();
+                        System.out.println("Ingrese la cantidad del producto: ");
+                        cantidadProducto = entrada.nextInt();
+                        //mi operacion
+                        resultado = precio * cantidadProducto;
+                        compra  += resultado;        
+                    }
+                    System.out.println("El total de la compra es: " + compra);
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                default:
+                    System.out.println("Solo opciones permiitidas del 1 al 14");
+                    break;
+            }
             
 
             //aqui la pregunta
